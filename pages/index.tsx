eea6720b4f95b4d01c1f1f7687820cc15c4c7c8d
Prima-Sunil <sunil.patidar@primathon.in>
2025-01-30 13:50:13 +0530

    task 13 Fetching Data with getInitialProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import React from "react";
+interface user{
+  name:string;
+  bio:string;
+}
+const Home = ({ data }: { data: user }) => {
   return (
     <div>
-
+      <h1>Next.js getInitialProps Example</h1>
+      <p>Data from API: {data.name}</p>
     </div>
-      );
-}
+  );
+};
+
+// Fetching data using getInitialProps
+Home.getInitialProps = async () => {
+  const res = await fetch("https://api.github.com/users/octocat");
+  const json = await res.json();
+  return { data: json };
+};
+
+export default Home;
